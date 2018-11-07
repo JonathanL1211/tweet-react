aaada9f24d55f4c9707c02f536c996e37de4e012
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-11-07 17:21:08 +0800

    add: kayneWest profile pic and styling

--- script.jsx
+++ script.jsx
@@ -3,10 +3,12 @@ class Tweets extends React.Component{
         console.log("TWEETS THIS.PROPS: ", this.props);
         return(
             <div>
-                <img src={this.props.details.profile_background_image_url}/>
-                <h3><span>{this.props.details.user.name} </span><span>@{this.props.details.user.screen_name}</span></h3>
-                <p>{this.props.details.text}</p>
-                <p><span>Retweets: {this.props.details.retweet_count}  | </span><span>Liked: {this.props.details.favorite_count}</span></p>
+                <img src="ye.jpg"/>
+                <div className="tweet-contents">
+                    <h3><span>{this.props.details.user.name} </span><span>@{this.props.details.user.screen_name}</span></h3>
+                    <p>{this.props.details.text}</p>
+                    <p><span>Retweets: {this.props.details.retweet_count} | Liked: {this.props.details.favorite_count}</span></p>
+                </div>
                 <br/>
                 <hr/>
             </div>
@@ -19,12 +21,12 @@ class Container extends React.Component{
     render(){
         console.log("CONTAINER THIS.PROPS: ", this.props);
         let listTweets = this.props.items.map(items => {
-            return <Tweets details={items}/>
+            return <Tweets details={items} key={items.id}/>
         })
         return (
             <div className="wrapper">
                 <nav><h3>{tweets[0].user.name}</h3></nav>
-                <ul>
+                 <ul>
                     {listTweets}
                 </ul>
             </div>
